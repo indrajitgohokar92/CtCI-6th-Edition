d54308f28c3caea859a0de8ab6a90aceb00b3e52
Yogesh Darji <yogesh_darji99@yahoo.com>
2016-01-01 13:44:00 -0600

    Update QuestionB.java

--- Java/Ch 01. Arrays and Strings/Q1_06_String_Compression/QuestionB.java
+++ Java/Ch 01. Arrays and Strings/Q1_06_String_Compression/QuestionB.java
@@ -7,7 +7,7 @@ public class QuestionB {
 		for (int i = 0; i < str.length(); i++) {
 			countConsecutive++;
 			
-			/* If next character is different than current, append this char to result.*/
+			/* If next character is different than current, append this char to the result.*/
 			if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
 				compressed.append(str.charAt(i));
 				compressed.append(countConsecutive);
